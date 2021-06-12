07cabca700cf269e39b26933960534ac5b02f8f9
DygelnyyKirill <dygelniy.1994kirill@gmail.com>
2021-06-12 14:47:36 +0300

    viewing history game TicTacToe

--- src/Game.js
+++ src/Game.js
@@ -8,12 +8,13 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         console.log('new', current)
         const squares = current.squares.slice();
@@ -25,19 +26,27 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
     }
 
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        })
+    };
+
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         console.log(current)
         const winner = calculateWinner(current.squares);
         const moves = history.map((step, move) => {
             const desc = move ? 'Go to move #' + move : 'To the start of the game';
             return (
-                <li>
+                <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             )
@@ -49,7 +58,6 @@ class Game extends React.Component {
         } else {
             status = 'Next move: ' + (this.state.xIsNext ? 'X' : 'O');
         }
-        
     
         return (
         <div className="game">
